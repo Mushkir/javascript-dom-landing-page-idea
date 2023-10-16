2f45495abe0a5c84c6ae44ecf92ff2152f03bb7c
Mushkir <mushkirmohamed@gmail.com>
2023-10-16 16:57:25 +0530

    Error fixed

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import {resolve} from 'path'
+import { defineConfig } from "vite"
+import {resolve} from "path"
 
 export default defineConfig({
     base: "/javascript-dom-landing-page-idea/",
