9af470e5a39b24a6d78289c5a3d5faa0e07f99b4
Mushkir <mushkirmohamed@gmail.com>
2023-10-16 15:36:09 +0530

    Playlist page done

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
+import {resolve} from 'path'
 
 export default defineConfig({
-    base: "/javascript-dom-landing-page-idea/"
+    base: "/javascript-dom-landing-page-idea/",
+
+    build: {
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname,'index.html'),
+                playlist: resolve(__dirname,'playlist.html'),
+            }
+        }
+    }
 })
